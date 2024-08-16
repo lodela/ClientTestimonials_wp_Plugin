297280b3fb301946a651375aebb7d2f3518234cf
Norberto <lodela@users.noreply.github.com>
2024-08-16 11:39:40 -0600

    Update script.js
    Jquery para  la funcionalidad del carrusel.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
-// script.js
 jQuery(document).ready(function($) {
-    // JavaScript code to handle the testimonials carousel
+    var $carousel = $('.ct-testimonials-carousel');
+    var interval = 5000; // Change slide every 5 seconds
+    var index = 0;
+    var slides = $carousel.find('.ct-testimonial');
+    var totalSlides = slides.length;
+
+    function showSlide(i) {
+        slides.hide().eq(i).fadeIn();
+    }
+
+    setInterval(function() {
+        index = (index + 1) % totalSlides;
+        showSlide(index);
+    }, interval);
+
+    showSlide(index);
 });
